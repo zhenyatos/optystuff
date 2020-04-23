37cd05da957fd4192ae2ce7a14534bc75923ba62
zhenyatos <zhenyatos999@gmail.com>
2020-04-23 14:29:18 +0300

    #13 Comment Numerical module

--- Numerical/derivative.h
+++ Numerical/derivative.h
@@ -4,11 +4,32 @@
 
 namespace nric
 {
-	const double EPS = 0.000001;
+	const double EPS = 0.000001;	// Default epsilon value
 
+	/*
+		ANALYSIS: derivative calculation
+		@param	fun - function of 1 variable
+		@param	x - point
+		@param	eps - sort of precision
+		@return derivative value
+	*/
 	double derivative(std::function<double(double)> fun, double x, double eps = EPS);
 
+	/*
+		ANALYSIS: gradient calculation
+		@param	fun - multivariable function
+		@param	x - point
+		@param	eps - sort of precision
+		@return	gradient vector
+	*/
 	vec gradient(vecfun fun, vec x, double eps = EPS);
 
+	/*
+		ANALYSIS: hessian calculation
+		@param	fun - multivariable function
+		@param	x - point
+		@param	eps - sort of precision
+		@return	hessian matrix
+	*/
 	mat hessian(vecfun fun, vec x, double eps = EPS);
 }
--- Numerical/mat.h
+++ Numerical/mat.h
@@ -4,6 +4,10 @@
 
 namespace nric
 {
+	/*
+		This class represents simple numerical computation
+		matrix (unresizeable)
+	*/
 	class mat
 	{
 	private:
@@ -13,13 +17,31 @@ namespace nric
 		double** initVals(int M, int N);
 
 	public:
-		mat(int N, int M);
+		/*
+			Basic constructor
+			@param	M - number of rows
+			@param	N - number of columns
+		*/
+		explicit mat(int N, int M);
+		/*
+			This constructor is used to initialize like:
+			mat E = {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
+		*/
 		mat(std::initializer_list<std::initializer_list<double>> list);
 		mat(const mat& other);
 		mat& operator=(const mat& other);
 		~mat();
 
+		/*
+			Part of nric::mat matrix-like behaviour
+			to set data
+		*/
 		double& operator()(int i, int j);
+		/*
+			Part of nric::mat matrix-like behaviour
+			to get data from const nric::mat
+			double x = E(0, 1);
+		*/
 		double operator()(int i, int j) const;
 		int M() const;
 		int N() const;
