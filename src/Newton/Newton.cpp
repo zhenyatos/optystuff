b3de8727f867312a8653cabec8fb55d3bfbae800
zhenyatos <zhenyatos999@gmail.com>
2020-04-26 12:59:26 +0300

    #19 Update Newton module
    -Adaptive step algorithm implemented
    -Comments updated
    -Tested

--- src/Newton/Newton.cpp
+++ src/Newton/Newton.cpp
@@ -11,20 +11,43 @@ std::ostream& operator<<(std::ostream& stream, const Newton::Result& res)
 	return stream;
 }
 
+Newton::Newton(double delta, double epsilon)
+{
+	if (delta <= 0 || delta >= 1)
+		delta_ = 0.5;
+	else
+		delta_ = delta;
+
+	if (epsilon <= 0 || epsilon >= 1)
+		epsilon_ = 0.5;
+	else
+		epsilon_ = epsilon;
+}
+
 Newton::Result Newton::optimize(nric::vecfun fun, nric::vec init, double prec)
 {
 	nric::vec& x0 = init;
 	nric::vec x(x0.dim());
 
+	nric::vec grad = nric::gradient(fun, x0);
 	nric::vec h = nric::solve(nric::hessian(fun, x0), nric::gradient(fun, x0));
-	x = x0 - h;
+	
+	double alpha = 1;
+	while (fun(x0 - alpha * h) - fun(x0) > -epsilon_ * alpha * (grad * h))
+		alpha *= delta_;
+	x = x0 - alpha * h;
 
 	int iter = 1;
 	while (nric::norm(x - x0) >= prec)
 	{
 		x0 = x;
-		h = nric::solve(nric::hessian(fun, x0), nric::gradient(fun, x0));
-		x = x0 - h;
+		grad = nric::gradient(fun, x0);
+		h = nric::solve(nric::hessian(fun, x0), grad);
+	
+		alpha = 1;
+		while (fun(x0 - alpha * h) - fun(x0) > -epsilon_ * alpha * (grad * h))
+			alpha *= delta_;
+		x = x0 - alpha * h;
 		iter++;
 	}
 
